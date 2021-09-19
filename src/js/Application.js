b8cc1c4180a246e45f418d66a15098beafacc33d
Hristiyan Ivanov <civanov.me@gmail.com>
2021-09-19 11:07:28 +0300

    feat: add boilerplate

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,6 @@
 import EventEmitter from "eventemitter3";
+import Card from "./Card";
+import Notification from "./Notification";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -10,9 +12,26 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
+    const pizzas = [
+      {
+        type: Card.types.HAWAIIAN,
+        price: 8.99,
+      },
+      {
+        type: Card.types.PEPPERONI,
+        price: 9.99,
+      },
+      {
+        type: Card.types.MARGHERITA,
+        price: 7.99,
+      },
+    ];
+
+    pizzas.forEach((pizza) => {
+      const card = new Card({ ...pizza });
+      card.render();
+
+      document.querySelector(".main").appendChild(card.container);
     });
 
     this.emit(Application.events.READY);
